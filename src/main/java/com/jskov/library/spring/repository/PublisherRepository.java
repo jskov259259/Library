0ae650b6434862a77be1866cd166a47d49aab11b
Skov <jskov259259@rambler.ru>
2022-07-18 17:51:11 +0300

    GenreRepository, PublisherRepository

---
+++ src/main/java/com/jskov/library/spring/repository/PublisherRepository.java
@@ -0,0 +1,18 @@
+package com.jskov.library.spring.repository;
+
+import com.jskov.library.domain.Publisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+    List<Publisher> findByNameContainingIgnoreCaseOrderByName(String name);
+
+    Page<Publisher> findByNameContainingIgnoreCaseOrderByName(String name, Pageable pageable);
+
+}
