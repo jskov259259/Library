5d53e309a7ca64c1470f46d4ead449b42d10cfd3
Skov <jskov259259@rambler.ru>
2022-07-20 16:20:05 +0300

    content, genre, topBooks methods, GeneralDao

--- src/main/java/com/jskov/library/spring/repository/BookRepository.java
+++ src/main/java/com/jskov/library/spring/repository/BookRepository.java
@@ -1,10 +1,35 @@
 package com.jskov.library.spring.repository;
 
 import com.jskov.library.domain.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByNameContainingIgnoreCaseOrAuthorFioContainingIgnoreCaseOrderByName(String name, String fio, Pageable pageable);
+
+    @Query("select new com.jskov.library.domain.Book(b.id, b.name, b.pageCount, b.isbn, b.genre, b.author, b.publisher, b.publishYear, b.image, b.descr, b.viewCount, b.totalRating, b.totalVoteCount, b.avgRating) from Book b")
+    Page<Book> findAllWithoutContent(Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Book b set b.content=:content where b.id =:id")
+    void updateContent(@Param("content") byte[] content, @Param("id") long id);
+
+    @Query("select new com.jskov.library.domain.Book(b.id, b.image) from Book b")
+    List<Book> findTopBooks(Pageable pageable);
+
+    @Query("select new com.jskov.library.domain.Book(b.id, b.name, b.pageCount, b.isbn, b.genre, b.author, b.publisher, b.publishYear, b.image, b.descr, b.viewCount, b.totalRating, b.totalVoteCount, b.avgRating) from Book b where b.genre.id=:genreId")
+    Page<Book> findByGenre(@Param("genreId") long genreId, Pageable pageable);
+
+    @Query("select b.content FROM Book b where b.id = :id")
+    byte[] getContent(@Param("id") long id);
 
 }
